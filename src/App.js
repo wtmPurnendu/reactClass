2c4dfd87727cd2d86d417fe3cf20d09c549d12d0
wtmPurnendu <wtm.purnendu@gmail.com>
2022-04-25 12:15:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div>
+      <h1>This is footer</h1>
+    </div>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
